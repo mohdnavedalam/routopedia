b5a30e8110f4c559a871161f53c1798ae4d48c12
Mohd Naved Alam <mohdnavedalam1987@gmail.com>
2024-07-07 02:34:32 +0530

    added routes - home, about

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,23 @@ import Header from './Components/Layout/Header';
 import Footer from './Components/Layout/Footer';
 import Navbar from './Components/Navigation/Navbar';
 import Dropdown from './Components/Dropdowns/DropdownButton';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home';
+import About from './Pages/About';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <div>
-    <Header />
-    <Navbar />
-    <Footer />
+    <BrowserRouter>
+      <Header />
+      <Navbar />
+      <Footer />
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/About' element={<About />}></Route>
+      </Routes>
+    </BrowserRouter>
   </div>
 );
 
