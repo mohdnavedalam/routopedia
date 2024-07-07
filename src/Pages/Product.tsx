8feb92639fd052410e92dbb1b5c3da61c49cc9c2
Mohd Naved Alam <mohdnavedalam1987@gmail.com>
2024-07-08 02:33:54 +0530

    Navigate component

--- src/Pages/Product.tsx
+++ src/Pages/Product.tsx
@@ -1,7 +1,19 @@
-import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { Link, Navigate, useNavigate } from "react-router-dom";
 
 const Product = () => {
     const navigate = useNavigate();
+    const [goToProduct, setGoToProduct] = useState(() => {
+        return false;
+    });
+    const handleSetGoToProduct = () => {
+        setGoToProduct((prevState: any) => {
+            return {
+                ...prevState,
+                goToProduct: true
+            }
+        });
+    };
     return (
         <>
         <div>Product</div>
@@ -11,6 +23,9 @@ const Product = () => {
             <Link to={`/Product/Details/5`}>
                 <button>Navigate to Product Details - 5</button>
             </Link>
+            <br />
+            {goToProduct && <Navigate to="/Product/Details/3" />}
+            <button onClick={handleSetGoToProduct}>Navigate to Product Details - 3 (useState)</button>
         </div>
         </>
     );
